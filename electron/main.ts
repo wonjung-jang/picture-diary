5c0e992ae3b5fed2b231db53fa7646351f68d339
장원정 <jang_expedition@gmail.com>
2025-04-29 11:47:11 +0900

    초기 설정: Next.js + Electron + TypeScript + Tailwind CSS

---
+++ electron/main.ts
@@ -0,0 +1,44 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as isDev from "electron-is-dev";
+
+let mainWindow: BrowserWindow | null = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  const startUrl = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../.next/server/pages/index.html")}`;
+
+  mainWindow.loadURL(startUrl);
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
